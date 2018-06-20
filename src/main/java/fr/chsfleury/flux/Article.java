b5c5683ae1601dd3aaa1589605585f7f1dfdaef7
Charles Fleury <charles@brandandcelebrities.com>
2018-06-20 19:36:38 +0200

    first try

---
+++ src/main/java/fr/chsfleury/flux/Article.java
@@ -0,0 +1,17 @@
+package fr.chsfleury.flux;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class Article {
+
+    private String title;
+    private String subtitle;
+    private String content;
+    private List<String> tags;
+
+}
--- src/main/java/fr/chsfleury/flux/Flux.java
+++ src/main/java/fr/chsfleury/flux/Flux.java
@@ -1,62 +1,21 @@
 package fr.chsfleury.flux;
 
-import com.rometools.rome.feed.synd.SyndFeed;
-import com.rometools.rome.io.SyndFeedInput;
-import com.rometools.rome.io.XmlReader;
-import lombok.extern.slf4j.Slf4j;
-import ratpack.exec.Promise;
-import ratpack.handling.Chain;
-import ratpack.handling.Context;
-import ratpack.http.client.HttpClient;
-import ratpack.server.RatpackServer;
+import lombok.Getter;
 
-import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
-@Slf4j
+@Getter
 public class Flux {
 
-    /*
-     * https://korben.info/feed
-     * https://www.lesnumeriques.com/rss.xml
-     */
+    private String title;
+    private String subtitle;
 
-    public static void main(final String[] args) throws Exception {
-        RatpackServer.start(server -> server.handlers(Flux::endpoints));
-    }
+    private List<Article> articles;
 
-    private static void endpoints(Chain chain) {
-        chain
-            .get(Flux::defaultPage)
-            .get("read", Flux::read);
-    }
-
-    private static void defaultPage(Context ctx) {
-        ctx.render("Hello World!");
-    }
-
-    private static void read(Context ctx) {
-        ctx.render(
-            doRead(ctx)
-                .map(SyndFeed::toString)
-                .mapError(Flux::renderError)
-        );
-    }
-
-    private static String renderError(Throwable t) {
-        log.error("error", t);
-        return t.getMessage();
-    }
-
-    private static Promise<SyndFeed> doRead(Context ctx) {
-        return Promise
-            .sync(() -> new URI(ctx.getRequest().getQueryParams().get("flux")))
-            .flatMap(uri -> ctx
-                .get(HttpClient.class)
-                .get(uri)
-            ).map(response -> {
-                XmlReader reader = new XmlReader(response.getBody().getInputStream());
-                SyndFeedInput input = new SyndFeedInput();
-                return input.build(reader);
-            });
+    public Flux(String title, String subtitle) {
+        this.title = title;
+        this.subtitle = subtitle;
+        this.articles = new ArrayList<>();
     }
 }
